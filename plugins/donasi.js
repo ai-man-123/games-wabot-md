8bf81685a6357ecc478c94e0dc4f86b28eb84eb9
AIMAN *͘⁴̅⁰͍⁴̵〆* <Lynxaiman@yahoo.com>
2022-12-13 12:00:12 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,11 @@
 let handler =  m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [085713964963]
-│ • Telkomsel [081319944687]
+│ -
 ╰────
 
 ╭─「 Donasi • Non Pulsa 」
-│ • https://saweria.co/BochilGaming
-│ • Gopay [085713964963]
+│ • -
+│ • -
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
